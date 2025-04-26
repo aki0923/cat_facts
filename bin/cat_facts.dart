1cdb213ede53119db3fa8203b25748b191f1d4ad
aki0923 <abibilaevaknazar34@gmail.com>
2025-04-26 18:47:45 +0600

    base structure

--- bin/cat_facts.dart
+++ bin/cat_facts.dart
@@ -1,5 +1,3 @@
-import 'package:cat_facts/cat_facts.dart' as cat_facts;
-
-void main(List<String> arguments) {
-  print('Hello world: ${cat_facts.calculate()}!');
+void main(List<String> args) async {
+  print('app that gets cat facts"');
 }
